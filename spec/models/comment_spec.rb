8d0f72958af6f2741916f754d6e2b3aab68c99c3
helarqjsc <helarqjsc@gmail.com>
2012-06-14 17:19:00 +0600

    added tests for Question model; and few changes in Comment model's tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,18 +2,14 @@ require 'spec_helper'
 
 describe Comment do
 
-	it 'belongs to question' do 
+	it 'belongs to a question' do 
 		comment = FactoryGirl.create(:comment)	
 		comment.question.should be_true
 	end
 	
-	it 'can be only added to an answered question' do
-		question1 = FactoryGirl.create(:question, has_answer: false, answer_content: '')
-		question2 = FactoryGirl.create(:question)
-		comment = FactoryGirl.create(:comment, question_id: nil)	
-		
-		question1.add_comment(comment).should be_false
-		question2.add_comment(comment).should be_true
+	it 'should have a body' do 
+		comment = FactoryGirl.create(:comment, content: '')
+		comment.should_not be_valid		
 	end
-	
+	 
 end
