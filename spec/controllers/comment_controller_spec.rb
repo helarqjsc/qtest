7827ab0a6b7aa508691983af1e62a4313c5bd49e
helarqjsc <helarqjsc@gmail.com>
2012-06-14 23:42:14 +0600

    created tests for comment controller

--- spec/controllers/comment_controller_spec.rb
+++ spec/controllers/comment_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe CommentController do
-
+	render_views
+	
+	it 'should create comment in db on #add action' do
+		post 'add', {content: 'lorem ipsum dolor sit amet'}
+		Post.all.should_not be_empty
+	end
 end
--- spec/controllers/question_controller_spec.rb
+++ spec/controllers/question_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe QuestionController do
-
+	it {respond_to :show_all}
+	it {respond_to :add}
 end
